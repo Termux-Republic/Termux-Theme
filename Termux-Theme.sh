528a68734f9effaeaaa6a49db455ff6a98743d7c
FKB12 <feketekistof541@gmail.com>
2026-04-04 12:26:33 +0200

    Add new themes to Termux-Theme.sh

--- Termux-Theme.sh
+++ Termux-Theme.sh
@@ -185,6 +185,118 @@ color14=#00ffff
 color15=#ffffff
 "; }
 
+# --- NEW THEMES ---
+
+catppuccin_mocha() { apply "
+background=#1e1e2e
+foreground=#cdd6f4
+cursor=#f5e0dc
+color0=#45475a
+color1=#f38ba8
+color2=#a6e3a1
+color3=#f9e2af
+color4=#89b4fa
+color5=#f5c2e7
+color6=#94e2d5
+color7=#bac2de
+color8=#585b70
+color9=#f38ba8
+color10=#a6e3a1
+color11=#f9e2af
+color12=#89b4fa
+color13=#f5c2e7
+color14=#94e2d5
+color15=#a6adc8
+"; }
+
+tokyo_night() { apply "
+background=#1a1b26
+foreground=#c0caf5
+cursor=#c0caf5
+color0=#15161e
+color1=#f7768e
+color2=#9ece6a
+color3=#e0af68
+color4=#7aa2f7
+color5=#bb9af7
+color6=#7dcfff
+color7=#a9b1d6
+color8=#414868
+color9=#f7768e
+color10=#9ece6a
+color11=#e0af68
+color12=#7aa2f7
+color13=#bb9af7
+color14=#7dcfff
+color15=#c0caf5
+"; }
+
+rose_pine() { apply "
+background=#191724
+foreground=#e0def4
+cursor=#524f67
+color0=#26233a
+color1=#eb6f92
+color2=#31748f
+color3=#f6c177
+color4=#9ccfd8
+color5=#c4a7e7
+color6=#ebbcba
+color7=#e0def4
+color8=#6e6a86
+color9=#eb6f92
+color10=#31748f
+color11=#f6c177
+color12=#9ccfd8
+color13=#c4a7e7
+color14=#ebbcba
+color15=#e0def4
+"; }
+
+gruvbox_light() { apply "
+background=#fbf1c7
+foreground=#3c3836
+cursor=#3c3836
+color0=#fbf1c7
+color1=#cc241d
+color2=#98971a
+color3=#d79921
+color4=#458588
+color5=#b16286
+color6=#689d6a
+color7=#7c6f64
+color8=#928374
+color9=#9d0006
+color10=#79740e
+color11=#b57614
+color12=#076678
+color13=#8f3f71
+color14=#427b58
+color15=#3c3836
+"; }
+
+material_dark() { apply "
+background=#212121
+foreground=#eeffff
+cursor=#ffcc02
+color0=#000000
+color1=#b71c1c
+color2=#1b5e20
+color3=#f57f17
+color4=#1a237e
+color5=#880e4f
+color6=#006064
+color7=#eceff1
+color8=#546e7a
+color9=#f07178
+color10=#c3e88d
+color11=#ffcb6b
+color12=#82aaff
+color13=#c792ea
+color14=#89ddff
+color15=#ffffff
+"; }
+
 echo "Available themes:"
 echo "  1) Solarized Dark"
 echo "  2) Solarized Light"
@@ -194,6 +306,11 @@ echo "  5) Gruvbox Dark"
 echo "  6) Monokai"
 echo "  7) One Dark"
 echo "  8) Default Termux"
+echo "  9) Catppuccin Mocha"
+echo " 10) Tokyo Night"
+echo " 11) Rosé Pine"
+echo " 12) Gruvbox Light"
+echo " 13) Material Dark"
 echo ""
 read -p "Pick a number: " choice
 
@@ -206,5 +323,10 @@ case $choice in
     6) monokai ;;
     7) one_dark ;;
     8) default_termux ;;
+    9) catppuccin_mocha ;;
+   10) tokyo_night ;;
+   11) rose_pine ;;
+   12) gruvbox_light ;;
+   13) material_dark ;;
     *) echo "Invalid choice." ;;
 esac
